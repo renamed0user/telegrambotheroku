04f2fe06fc1a7f9711040d155c74b9dbdbc0aba2
Andriy <you@example.comandriyhalalay@gmail.com>
2022-03-16 19:14:10 +0200

    make it better

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import logging
 import os
 import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
