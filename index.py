e72e1be523a53abcf90e1bcca80c795d7b42ec67
Andriy <you@example.comandriyhalalay@gmail.com>
2022-03-16 20:18:32 +0200

    make it better

--- index.py
+++ index.py
@@ -20,7 +20,7 @@ def start(update, context):
     update.message.reply_text(update.message.message_id)
     bot = telebot.TeleBot('5288239676:AAH40vF7Ymn41ODeJZYbTZKE-Wg1EbgkOoI')
     update.message.reply_text(update.message.chat_id)
-    bot.send_message(update.message.chat_id, "Choose a language\nВиберіть мову\nВыберите язык".format(update.message.from_user, bot.get_me()))
+    bot.send_message(update.message.chat_id, 'Choose a language\nВиберіть мову\nВыберите язык'.format(update.message.from_user, bot.get_me()))
     markup = types.InlineKeyboardMarkup()
     update.message.reply_text(update.message.message_id)
     b1=types.InlineKeyboardButton(text='English', callback_data='English')
