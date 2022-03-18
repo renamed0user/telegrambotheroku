f8d11db42fe7ae01fa885eac6acd67dc97679ab9
Andriy <you@example.comandriyhalalay@gmail.com>
2022-03-18 11:00:05 +0200

    make it better

--- index.py
+++ index.py
@@ -49,7 +49,6 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_error_handler(error)
     updater.start_webhook(listen="0.0.0.0",port=PORT,url_path=TOKEN,webhook_url=APP_NAME + TOKEN)
     updater.idle()
