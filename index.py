0e1fcc8cb327c91e9bb75f6424dfd3dd8e4e25b5
Andriy <you@example.comandriyhalalay@gmail.com>
2022-03-18 22:08:31 +0200

    make it better

--- index.py
+++ index.py
@@ -20,7 +20,7 @@ def start(update, context):
     for each in ["English", "Українська"]:
         button_list.append(InlineKeyboardButton(each, callback_data=each))
     update.message.reply_text('Hi!')
-    reply_markup = InlineKeyboardMarkup(button_list[:2])
+    markup = InlineKeyboardMarkup(button_list[:2])
     update.message.reply_text('Hi!')
     update.message.reply_text(text="Choose a language\nВиберіть мову", reply_markup=markup, parse_mode='HTML')
 
