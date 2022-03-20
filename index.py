aafd69049305e0089c10a7393cc5230c9ae5ba31
Andriy <you@example.comandriyhalalay@gmail.com>
2022-03-21 00:42:36 +0200

    make it better

--- index.py
+++ index.py
@@ -33,12 +33,12 @@ def button_press(update, context):
     button_list = []
     if update.callback_query.data=='English':
         for each in ["Weather"]:
-            button_list.append([InlineKeyboardButton(each, callback_data=each)])
-        update.message.reply_text(text="You choose English\nSelect tusk", reply_markup=InlineKeyboardMarkup(button_list[:2]), parse_mode='HTML')
+            button_list.append([KeyboardButton(each, request_location=true)])
+        update.message.reply_text(text="You choose English\nSelect tusk", reply_markup=ReplyKeyboardMarkup(button_list[:2]), parse_mode='HTML')
     elif update.callback_query.data=='Українська':
         for each in ["Погода"]:
-            button_list.append([InlineKeyboardButton(each, callback_data=each)])
-        update.message.reply_text(text="Ви вибрали Українську\nСлава Україні!\nСмерть москалям!\nОберіть завдання", reply_markup=InlineKeyboardMarkup(button_list[:2]), parse_mode='HTML')
+            button_list.append([KeyboardButton(each, request_location=true)])
+        update.message.reply_text(text="Ви вибрали Українську\nСлава Україні!\nСмерть москалям!\nОберіть завдання", reply_markup=ReplyKeyboardMarkup(button_list[:2]), parse_mode='HTML')
     elif update.callback_query.data=='Weather' or update.callback_query.data=='Погода':
         update.message.reply_text(text=('latitude: %s; longitude: %s'%(update.message.location.latitude,update.message.location.longitude)))
 
