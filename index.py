9dd6877e52e5561d82d659a0212e64d13e1f3db7
Andriy <you@example.comandriyhalalay@gmail.com>
2022-03-21 15:05:07 +0200

    make it better

--- index.py
+++ index.py
@@ -49,7 +49,8 @@ def button_press(update, context):
 def get_weather(update, context):
     Lat=update.message.location.latitude
     Lng=update.message.location.longitude
-    weather=owm.weather_manager().weather_at_place(owm.weather_around_coords(Lat, Lng)).weather
+    mg=owm.weather_manager()
+    weather=mg.weather_at_place(mg.weather_around_coords(Lat, Lng)).weather
     context.bot.send_message(update.message.chat_id,text=weather)
 
 def main():
